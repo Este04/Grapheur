14da8f4ab143262eb97610b720f9b243de9dbcec
Robin Vierlinck <robin.vierlinck@gmail.com>
2024-05-16 20:53:27 +0200

    Nettoyage

--- Utilisé/signal_SH.py
+++ Utilisé/signal_SH.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib . pyplot as plt
 
-y = [*np.zeros(301), *np.ones(1000-301)]
+y = [*np.zeros(101), *np.ones(1000-101)]
 print(len(y))
 integrateur = np.linspace(0, 1, int(500*5/6))
 
-y = [*y[:301], *integrateur[301:301+20], *np.ones(1000-301-20)*integrateur[320]]
+y = [*y[:101], *integrateur[101:101+20], *np.ones(1000-101-20)*integrateur[120]]
 # print(np.shape(y), y)
 x = np.linspace(0, 10, 1000)
 plt.plot(x, y)
 plt.axis('off')
-
-# plt.savefig("Demo_signalSH.pdf")
+plt.ylim(-0.2, 1.2)
+# plt.savefig("Demo_signal_SH.pdf")
 plt.show()
