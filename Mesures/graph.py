74dd8b226ba614e74eae8986ea15bf5ed6f89d17
Este04 <esteban.tello@outlook.com>
2024-05-17 17:35:42 +0200

    up

--- Mesures/graph.py
+++ Mesures/graph.py
@@ -8,7 +8,7 @@ def bascule() :
     file_location = "Data/Bascule.txt"
     data = np.loadtxt(file_location, delimiter="\t", skiprows=1)
 
-    t = data[:, 0] * 1000
+    t =  data[:, 0] * 1000
     V1 = data[:, 1]
     V2 = data[:, 2]
 
@@ -26,7 +26,35 @@ def bascule() :
     plt.legend(loc=(1.05, 0.85))
 
     plt.tight_layout()
-    plt.savefig(filename + '.pdf')
+    # plt.savefig(filename + '.pdf')
     plt.show()
 
-bascule()
+
+def oscillateur1() :
+    filename = "mesures_oscillateur1"
+
+    file_location = "Data/Oscillateur_1.txt"
+    data = np.loadtxt(file_location, delimiter="\t", skiprows=1)
+
+    t =  data[:400, 0] * 1000
+    V1 = data[:400, 1]
+    Vo = data[:400, 2]
+    V2 = data[:400, 3]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(t, V2, label="$V_{in_+}$ [V]", color="g")
+    plt.plot(t, V1, label="$V_{in_-}$ [V]", color="b")
+    plt.plot(t, Vo, label="$V_{out}$ [V]", color="r")
+    plt.grid(True)
+
+    plt.xlabel("Temps [ms]")
+    plt.ylabel("Tension [V]")
+    # plt.title("Tension en fonction de la distance")
+    plt.legend(loc=(1.05, 0.85))
+
+    plt.tight_layout()
+    # plt.savefig(filename + '.pdf')
+    plt.show()
+
+# bascule()
+# oscillateur1()
