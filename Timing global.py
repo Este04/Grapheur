4208eaedc5681fc8b54146f8d05e4ba95dd60b9a
Robin Vierlinck <robin.vierlinck@gmail.com>
2024-05-18 13:07:13 +0200

    update

--- Timing global.py
+++ Timing global.py
@@ -69,10 +69,12 @@ axs[3][1].set_ylabel('Bascule', fontsize=6)
 
 axs[4][1].plot(x, [0, *monostable_B], linewidth=0.8)
 axs[4][1].set_ylabel('Monostable B', fontsize=6)
+axs[4][1].vlines(x=[T * 142 / 500, T * 642 / 500], ymin=-1, ymax=0, linestyle='--', linewidth=0.8, color='r')
 
-axs[5][1].plot(x, [0, *SH], linewidth=0.8)
+axs[5][1].plot(x, [0, *integrateur], linewidth=0.5, linestyle=':')
+axs[5][1].plot(x, [0, *SH], linewidth=0.8, color='#1f77b4')
 axs[5][1].set_ylim((-0.5, 12))
-axs[5][1].vlines(x=[T * 142 / 500, T * 648 / 500], ymin=-1, ymax=15, linestyle='--', linewidth=0.8, color='r')
+axs[5][1].vlines(x=[T * 142 / 500, T * 642 / 500], ymin=-1, ymax=15, linestyle='--', linewidth=0.8, color='r')
 axs[5][1].set_ylabel('S/H', fontsize=6)
 
 
@@ -80,9 +82,21 @@ for i in range(6):
     if i!=5:
         axs[i][0].set_xticks([])
         axs[i][1].set_xticks([])
+        for j in range(2):
+            axs[i][j].spines['bottom'].set_linestyle(':')
+    if i != 0:
+        for j in range(2):
+            axs[i][j].spines['top'].set_visible(False)
+
     for j in range(2):
         axs[i][j].yaxis.set_label_coords(-0.2, 0.5)
+        axs[i][j].tick_params(axis='y', labelsize=8, width=0.5)
+        for spine in axs[i][j].spines.values():
+            spine.set_linewidth(0.5)
 
+for j in range(2):
+    axs[5][j].set_xticks([0, T, 2*T])
+    axs[5][j].set_xticklabels([0,'T', '2T'])
 # plt.text(x=T, y = 0, s="CHANGE PERIOD!")
-plt.savefig("TOUT.pdf")
+plt.savefig("TOUT_new_verion.pdf")
 plt.show()
