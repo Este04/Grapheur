d080d4c1ff6e38628b2bc8e7797539567c77388c
Este04 <esteban.tello@outlook.com>
2024-05-18 18:33:49 +0200

    pilou

--- Mesures/graph.py
+++ Mesures/graph.py
@@ -20,10 +20,12 @@ def simu_bascule() :
     plt.plot(t, V1, label="$V_{bascule}$ [V]", color="b")
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
-    plt.legend(loc=(1.05, 0.85))
+    plt.legend(loc='upper left', fontsize=20)
 
     plt.tight_layout()
     # plt.savefig(filename + '.pdf')
@@ -36,10 +38,10 @@ def simu_oscillateur1() :
     file_location = "Data/Oscillateur1.txt"
     data = np.loadtxt(file_location, delimiter="\t", skiprows=1)
 
-    t =  data[:275, 0] * 1000
-    V1 = data[:275, 1]
-    Vo = data[:275, 2]
-    V2 = data[:275, 3]
+    t =  [*np.zeros(1), *data[:275, 0] * 1000]
+    V1 = [*np.ones(1)*-1.340489e+01, *data[:275, 1]]
+    Vo = [*np.ones(1)*-1.340489e+01, *data[:275, 2]]
+    V2 = [*np.ones(1)*-1.147591e+01, *data[:275, 3]]
 
     plt.figure(figsize=(10, 6))
     plt.plot(t, V2, label="$V_{in_+}$ [V]", color="g")
@@ -47,8 +49,10 @@ def simu_oscillateur1() :
     plt.plot(t, Vo, label="$V_{out}$ [V]", color="r")
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
     plt.legend(loc='upper right', fontsize=20)
 
@@ -63,20 +67,22 @@ def simu_oscillateur2() :
     file_location = "Data/Oscillateur2.txt"
     data = np.loadtxt(file_location, delimiter="\t", skiprows=1)
 
-    t =  data[96:4585, 0] * 1000
-    V1 = data[96:4585, 1]
-    V2 = data[96:4585, 2]
+    t =  data[85:4585, 0] * 1000
+    V1 = data[85:4585, 1]
+    V2 = data[85:4585, 2]
 
     t -= t[0]
     # plt.figure(figsize=(10, 6))
-    plt.figure(figsize=(20, 6))
+    plt.figure(figsize=(10, 6))
     plt.plot(t, V2, label="$V_{oscillateur_1}$ [V]", color="b")
 
     plt.plot(t, V1, label="$V_{oscillateur_2}$ [V]", color="r")
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]" , fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
     plt.legend(loc='upper right', fontsize=20)
 
@@ -91,23 +97,25 @@ def simu_integrateur():
     file_location = "Data/Integrateur.txt"
     data = np.loadtxt(file_location, delimiter="\t", skiprows=1)
 
-    t =  data[600:, 0] * 1000
-    V1 = data[600:, 1]
-    V2 = data[600:, 2]
+    t =  data[600:5500, 0] * 1000
+    V1 = data[600:5500, 1]
+    V2 = data[600:5500, 2]
 
     t -= t[0]
 
-    plt.figure(figsize=(15, 6))
+    plt.figure(figsize=(10, 6))
     # plt.figure(figsize=(20, 6))
     plt.plot(t, V1, label="$V_{contrôle}$ [V]", color="b")
     plt.plot(t, V2, label="$V_{intégrateur}$ [V]", color="r")
 
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper left', fontsize=20)
 
     plt.tight_layout()
     # plt.savefig(filename + '.pdf')
@@ -122,9 +130,9 @@ def mesures_integrateur() :
     file_location2 = "Data/M1_4ms_5v.csv"
     data2 = np.loadtxt(file_location2, delimiter=",", skiprows=8)
 
-    t =  data1[175:4750, 1]
-    V1 = data1[175:4750, 2]
-    V2 = data2[175:4750, 2]
+    t =  data1[175:3200, 1]
+    V1 = data1[175:3200, 2]
+    V2 = data2[175:3200, 2]
 
     t = (t - t[0]) * 1000
 
@@ -132,16 +140,18 @@ def mesures_integrateur() :
     # plt.figure(figsize=(20, 6))
     plt.plot(t, V2, label="$V_{monostable\ A}$ [V]", color="b")
 
-    plt.plot(t, V1, label="$V_{integrateur}$ [V]", color="r")
+    plt.plot(t, V1, label="$V_{intégrateur}$ [V]", color="r")
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper left', fontsize=20)
 
     plt.tight_layout()
-    plt.savefig(filename + '.pdf')
+    # plt.savefig(filename + '.pdf')
     plt.show()
 
 def mesures_monostableA():
@@ -152,10 +162,10 @@ def mesures_monostableA():
     file_location2 = "Data/mesures_osc1&VplusmonoA.csv"
     data2 = np.loadtxt(file_location2, delimiter=",", skiprows=8)
 
-    t =  data1[1090:5665, 1]
-    V1 = data1[1090:5665, 2]
-    V2 = data1[1090:5665, 3]
-    V3 = data2[1090:5665, 3]
+    t =  data1[1090:4150, 1]
+    V1 = data1[1090:4150, 2]
+    V2 = data1[1090:4150, 3]
+    V3 = data2[1090:4150, 3]
 
     t = (t - t[0]) * 1000
 
@@ -167,10 +177,12 @@ def mesures_monostableA():
     plt.plot(t, V3, label="$V_{seuil}$ [V]", color="g")
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper left', fontsize=18)
 
     plt.tight_layout()
     # plt.savefig(filename + '.pdf')
@@ -192,19 +204,159 @@ def mesures_oscillateur1() :
     plt.plot(t, V1, label="$V_{oscillateur_1}$ [V]", color="b")
     plt.grid(True)
 
-    plt.xlabel("Temps [ms]")
-    plt.ylabel("Tension [V]")
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
     # plt.title("Tension en fonction de la distance")
-    plt.legend(loc='upper right')
+    plt.legend(loc='upper right', fontsize=20)
 
     plt.tight_layout()
     # plt.savefig(filename + '.pdf')
     plt.show()
 
+def mesures_oscillateur12() :
+    filename = "mesures_oscillateur1&2"
+
+    file_location1 = "Data/mesures_osc1&2.csv"
+    data1 = np.loadtxt(file_location1, delimiter=",", skiprows=8)
+
+    t =  data1[80:3110, 1]
+    V1 = data1[80:3110, 2]
+    V2 = data1[80:3110, 3]
+
+    t = (t - t[0]) * 1000
+
+    plt.figure(figsize=(10, 6))
+    # plt.figure(figsize=(20, 6))
+    plt.plot(t, V2, label="$V_{oscillateur_2}$ [V]", color="r")
+    plt.plot(t, V1, label="$V_{oscillateur_1}$ [V]", color="b")
+    plt.grid(True)
+
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
+    # plt.title("Tension en fonction de la distance")
+    plt.legend(loc='upper right', fontsize=20)
+
+    plt.tight_layout()
+    # plt.savefig(filename + '.pdf')
+    plt.show()
+
+
+def mesure_SH():
+    filename = "mesures_SH1m5"
+
+    file_location1 = "Data/I&NE_4ms_5v_1m5.csv"
+    data1 = np.loadtxt(file_location1, delimiter=",", skiprows=8)
+    file_location2 = "Data/SH_4ms_5v_1m5.csv"
+    data2 = np.loadtxt(file_location2, delimiter=",", skiprows=8)
+
+    n = 470
+
+    t =  data1[:, 1]
+    V1 = data1[:, 2]
+    V2 = np.array([*np.zeros(n), *data2[:len(data2)-n, 2]]) + 0.80
+    V3 = data1[:, 3]
+
+    t =   t[1550:4750]
+    V1 = V1[1550:4750]
+    V2 = V2[1550:4750]
+    V3 = V3[1550:4750]
+
+
+    t = (t - t[0]) * 1000
+
+    plt.figure(figsize=(10, 6))
+    # plt.figure(figsize=(20, 6))
+
+    plt.plot(t, V1, label="$V_{Intégrateur}$ [V]", color="r")
+    plt.plot(t, V3, label="$V_{monostable\ B}$ [V]", color="g")
+    plt.plot(t, V2, label="$V_{S/H}$ [V]", color="b")
+    plt.grid(True)
+
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
+    # plt.title("Tension en fonction de la distance")
+    plt.legend(loc='upper left', fontsize=18)
+
+    plt.tight_layout()
+    # plt.savefig(filename + '.pdf')
+    plt.show()
+
+
+def simu_osc2integrateur() :
+    filename = "simu_osc2integrateur"
+
+    file_location1 = "Data/Oscillateur2int.txt"
+    data1 = np.loadtxt(file_location1, delimiter="\t", skiprows=1)
+
+    t =  data1[200:, 0]
+    V1 = data1[200:, 1]
+    V2 = data1[200:, 2]
+
+    t = (t - t[0]) * 1000
+
+    plt.figure(figsize=(10, 6))
+    # plt.figure(figsize=(20, 6))
+    plt.plot(t, V2, label="$V_{intégrateur}$ [V]", color="b")
+    plt.plot(t, V1, label="$V_{oscillateur_2}$ [V]", color="r")
+    plt.grid(True)
+
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
+    # plt.title("Tension en fonction de la distance")
+    plt.legend(loc='upper right', fontsize=20)
+
+    plt.tight_layout()
+    # plt.savefig(filename + '.pdf')
+    plt.show()
+
+def mesures_crete() :
+    filename = "mesures_crete"
+
+    file_location1 = "Data/mesures_crete.csv"
+    data1 = np.loadtxt(file_location1, delimiter=",", skiprows=8)
+
+    t =  data1[:3000, 1]
+    V1 = data1[:3000, 2]
+    V2 = data1[:3000, 3]
+
+    t = (t - t[0]) * 1000
+
+    plt.figure(figsize=(10, 6))
+    # plt.figure(figsize=(20, 6))
+    plt.plot(t, V2, label="$V_{filtre}$ [V]", color="b")
+    plt.plot(t, V1, label="$V_{crête}$ [V]", color="r")
+    plt.grid(True)
+
+    plt.xlabel("Temps [ms]", fontsize=20)
+    plt.ylabel("Tension [V]", fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
+    # plt.title("Tension en fonction de la distance")
+    plt.legend(loc='upper right', fontsize=20)
+
+    plt.tight_layout()
+    # plt.savefig(filename + '.pdf')
+    plt.show()
+
+
+
+
 # simu_bascule()
 # simu_oscillateur1()
 # simu_oscillateur2()
-simu_integrateur()
+# simu_integrateur()
 # mesures_integrateur()
 # mesures_monostableA()
 # mesures_oscillateur1()
+# mesures_oscillateur12()
+# mesure_SH()
+# mesures_crete()
+# simu_osc2integrateur()
